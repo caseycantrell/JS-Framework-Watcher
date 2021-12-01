46fd9664528a03f1b814f3857b100cf41d8aadf2
Casey <cantrellcasey@gmail.com>
2021-12-01 15:19:10 -0600

    removes unnecessary chart libraries

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="home">
-    <h1>JS Framework Watcher</h1>
-    <h2>Vue</h2>
-    <p>Forks: {{ vueData.forks }}</p>
-    <p>Watchers: {{ vueData.subscribers_count }}</p>
-    <p>Stars: {{ vueData.watchers }}</p>
-    <h2>Angular</h2>
-    <p>Forks: {{ angularData.forks }}</p>
-    <p>Watchers: {{ angularData.subscribers_count }}</p>
-    <p>Stars: {{ angularData.watchers }}</p>
-    <h2>Ember</h2>
-    <p>Forks: {{ emberData.forks }}</p>
-    <p>Watchers: {{ emberData.subscribers_count }}</p>
-    <p>Stars: {{ emberData.watchers }}</p>
-    <h2>Svelte</h2>
-    <p>Forks: {{ svelteData.forks }}</p>
-    <p>Watchers: {{ svelteData.subscribers_count }}</p>
-    <p>Stars: {{ svelteData.watchers }}</p>
-    <h2>React</h2>
-    <p>Forks: {{ reactData.forks }}</p>
-    <p>Watchers: {{ reactData.subscribers_count }}</p>
-    <p>Stars: {{ reactData.watchers }}</p>
+    <div class="data">
+      <h1>JS Framework Watcher</h1>
+      <h2>Vue</h2>
+      <p>Forks: {{ vueData.forks }}</p>
+      <p>Watchers: {{ vueData.subscribers_count }}</p>
+      <p>Stars: {{ vueData.watchers }}</p>
+      <h2>Angular</h2>
+      <p>Forks: {{ angularData.forks }}</p>
+      <p>Watchers: {{ angularData.subscribers_count }}</p>
+      <p>Stars: {{ angularData.watchers }}</p>
+      <h2>Ember</h2>
+      <p>Forks: {{ emberData.forks }}</p>
+      <p>Watchers: {{ emberData.subscribers_count }}</p>
+      <p>Stars: {{ emberData.watchers }}</p>
+      <h2>Svelte</h2>
+      <p>Forks: {{ svelteData.forks }}</p>
+      <p>Watchers: {{ svelteData.subscribers_count }}</p>
+      <p>Stars: {{ svelteData.watchers }}</p>
+      <h2>React</h2>
+      <p>Forks: {{ reactData.forks }}</p>
+      <p>Watchers: {{ reactData.subscribers_count }}</p>
+      <p>Stars: {{ reactData.watchers }}</p>
+    </div>
   </div>
 </template>
 
@@ -32,7 +34,6 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
       vueData: [],
       angularData: [],
       emberData: [],
