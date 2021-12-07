da0ffa43cd63057e80f392041ab34f414bbcec03
Casey <cantrellcasey@gmail.com>
2021-12-06 21:15:55 -0600

    moves data onto cards and adds styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,108 +1,116 @@
 <template>
   <div class="home">
     <div class="data">
-      <h1>JS Framework Watcher</h1>
+      <div align="center">
+        <br />
+        <h1>JS FRAMEWORK WATCHER</h1>
+      </div>
       <br />
       <br />
       <div class="container-xl">
-        <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="row row-cols-2 row-cols-sm-5 g-4">
           <div class="col">
-            <div class="card mb-3" style="max-width: 540px">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img :src="vueThumb" class="img-fluid rounded-start" alt="" />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">Vue.js</h5>
-                    <p class="card-text">&#127860; Forks: {{ vueData.forks }}</p>
-                    <p class="card-text">&#128064; Watchers: {{ vueData.subscribers_count }}</p>
-                    <p class="card-text">&#x2b50; Stars: {{ vueData.watchers }}</p>
-                  </div>
-                </div>
+            <div class="card" style="width: 18rem">
+              <img class="card-img-top" :src="vueThumb" alt="Card image cap" />
+              <div class="card-body bg-info text-dark" align="center">
+                <h5 class="card-title">Vue</h5>
+                <p class="card-text"></p>
               </div>
+              <ul class="list-group list-group-flush" align="center">
+                <li class="list-group-item">&#127860; Forks: {{ vueData.forks }}</li>
+                <li class="list-group-item">&#128064; Watchers: {{ vueData.subscribers_count }}</li>
+                <li class="list-group-item">&#x2b50; Stars: {{ vueData.watchers }}</li>
+              </ul>
             </div>
           </div>
           <div class="col">
-            <div class="card mb-3" style="max-width: 540px">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img :src="angularThumb" class="img-fluid rounded-start" alt="" />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">Angular</h5>
-                    <p class="card-text">&#127860; Forks: {{ angularData.forks }}</p>
-                    <p class="card-text">&#128064; Watchers: {{ angularData.subscribers_count }}</p>
-                    <p class="card-text">&#x2b50; Stars: {{ angularData.watchers }}</p>
-                  </div>
-                </div>
+            <div class="card" style="width: 18rem">
+              <img class="card-img-top" :src="angularThumb" alt="Card image cap" />
+              <div class="card-body bg-info text-dark" align="center">
+                <h5 class="card-title">Angular</h5>
+                <p class="card-text"></p>
               </div>
+              <ul class="list-group list-group-flush" align="center">
+                <li class="list-group-item">&#127860; Forks: {{ angularData.forks }}</li>
+                <li class="list-group-item">&#128064; Watchers: {{ angularData.subscribers_count }}</li>
+                <li class="list-group-item">&#x2b50; Stars: {{ angularData.watchers }}</li>
+              </ul>
             </div>
           </div>
           <div class="col">
-            <div class="card mb-3" style="max-width: 540px">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img :src="emberThumb" class="img-fluid rounded-start" alt="" />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">Ember</h5>
-                    <p class="card-text">&#127860; Forks: {{ emberData.forks }}</p>
-                    <p class="card-text">&#128064; Watchers: {{ emberData.subscribers_count }}</p>
-                    <p class="card-text">&#x2b50; Stars: {{ emberData.watchers }}</p>
-                  </div>
-                </div>
+            <div class="card" style="width: 18rem">
+              <img class="card-img-top" :src="emberThumb" alt="Card image cap" />
+              <div class="card-body bg-info text-dark" align="center">
+                <h5 class="card-title">Ember</h5>
+                <p class="card-text"></p>
               </div>
+              <ul class="list-group list-group-flush" align="center">
+                <li class="list-group-item">&#127860; Forks: {{ emberData.forks }}</li>
+                <li class="list-group-item">&#128064; Watchers: {{ emberData.subscribers_count }}</li>
+                <li class="list-group-item">&#x2b50; Stars: {{ emberData.watchers }}</li>
+              </ul>
             </div>
           </div>
           <div class="col">
-            <div class="card mb-3" style="max-width: 540px">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img :src="svelteThumb" class="img-fluid rounded-start" alt="" />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">Svelte</h5>
-                    <p class="card-text">&#127860; Forks: {{ svelteData.forks }}</p>
-                    <p class="card-text">&#128064; Watchers: {{ svelteData.subscribers_count }}</p>
-                    <p class="card-text">&#x2b50; Stars: {{ svelteData.watchers }}</p>
-                  </div>
-                </div>
+            <div class="card" style="width: 18rem">
+              <img class="card-img-top" :src="svelteThumb" alt="Card image cap" />
+              <div class="card-body bg-info text-dark" align="center">
+                <h5 class="card-title">Svelte</h5>
+                <p class="card-text"></p>
               </div>
+              <ul class="list-group list-group-flush" align="center">
+                <li class="list-group-item">&#127860; Forks: {{ svelteData.forks }}</li>
+                <li class="list-group-item">&#128064; Watchers: {{ svelteData.subscribers_count }}</li>
+                <li class="list-group-item">&#x2b50; Stars: {{ svelteData.watchers }}</li>
+              </ul>
             </div>
           </div>
           <div class="col">
-            <div class="card mb-3" style="max-width: 540px">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img :src="reactThumb" class="img-fluid rounded-start" alt="" />
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">React</h5>
-                    <p class="card-text">&#127860; Forks: {{ reactData.forks }}</p>
-                    <p class="card-text">&#128064; Watchers: {{ reactData.subscribers_count }}</p>
-                    <p class="card-text">&#x2b50; Stars: {{ reactData.watchers }}</p>
-                  </div>
-                </div>
+            <div class="card" style="width: 18rem">
+              <img class="card-img-top" :src="reactThumb" alt="Card image cap" />
+              <div class="card-body bg-info text-dark" align="center">
+                <h5 class="card-title">React</h5>
+                <p class="card-text"></p>
               </div>
+              <ul class="list-group list-group-flush" align="center">
+                <li class="list-group-item">&#127860; Forks: {{ reactData.forks }}</li>
+                <li class="list-group-item">&#128064; Watchers: {{ reactData.subscribers_count }}</li>
+                <li class="list-group-item">&#x2b50; Stars: {{ reactData.watchers }}</li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
     </div>
-
-    <div align="center" v-if="forks.series[0]['values'].length > 4">
-      <zingchart :data="forks" :height="300" :width="800" />
-    </div>
-    <div align="center" v-if="watchers.series[0]['values'].length > 4">
-      <zingchart :data="watchers" :height="300" :width="800" />
-    </div>
-    <div align="center" v-if="stars.series[0]['values'].length > 4">
-      <zingchart :data="stars" :height="300" :width="800" />
+    <br />
+    <br />
+    <div class="mx-1 container-fluid">
+      <div class="mx-1 row">
+        <div class="card" style="">
+          <div class="card-body">
+            <h5 class="card-title" align="center">Forks</h5>
+            <div align="center" v-if="forks.series[0]['values'].length > 4">
+              <zingchart :data="forks" :height="300" :width="800" />
+            </div>
+          </div>
+        </div>
+        <div class="card" style="">
+          <div class="card-body">
+            <h5 class="card-title" align="center">Watchers</h5>
+            <div align="center" v-if="watchers.series[0]['values'].length > 4">
+              <zingchart :data="forks" :height="300" :width="800" />
+            </div>
+          </div>
+        </div>
+        <div class="card" style="">
+          <div class="card-body">
+            <h5 class="card-title" align="center">Stars</h5>
+            <div align="center" v-if="stars.series[0]['values'].length > 4">
+              <zingchart :data="stars" :height="300" :width="800" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -110,6 +118,24 @@
 <style>
 img {
   width: 150px;
+}
+body {
+  background: white;
+}
+.card {
+  /* Add shadows to create the "card" effect */
+  background: black;
+  box-shadow: 0 8px 22px 0 rgba(0, 0, 0, 0.44);
+  transition: 0.5s;
+  border-radius: 5px;
+}
+.card-title {
+  color: white;
+}
+
+/* On mouse-over, add a deeper shadow */
+.card:hover {
+  box-shadow: 0 12px 22px 0 rgba(0, 0, 0, 0.44);
 }
 </style>
 
@@ -137,7 +163,7 @@ export default {
       forks: {
         type: "bar",
         title: {
-          text: "Forks",
+          text: "",
         },
         series: [
           {
@@ -163,7 +189,7 @@ export default {
       watchers: {
         type: "bar",
         title: {
-          text: "Watchers",
+          text: "",
         },
         series: [
           {
@@ -189,7 +215,7 @@ export default {
       stars: {
         type: "bar",
         title: {
-          text: "Stars",
+          text: "",
         },
         series: [
           {
